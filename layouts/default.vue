b26c7cc2b231291cec6b375e282d6a95b0c5ca90
Demaria Woods <dpwoods@uh.edu>
2020-02-01 14:04:12 -0600

    added popper.js

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,7 @@
   src="https://code.jquery.com/jquery-3.4.1.slim.js"
   integrity="sha256-BTlTdQO9/fascB1drekrDVkaKd9PkwBymMlHOiG+qLI="
   crossorigin="anonymous"></script>
+  <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <AppFooter/>
 
