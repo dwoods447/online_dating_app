4ccc8163c3c4ce95169e1640dc559095bc3e8bf5
Demaria Woods <dwoods447@gmail.com>
2020-04-28 08:49:21 -0500

    changed port

--- middleware/services/api/api.js
+++ middleware/services/api/api.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 import config from '../../../nuxt.config'
 let api;
 if(process.env.NODE_ENV !== 'production'){
+  // api = axios.create({
+  //   baseURL: 'http://localhost:' + config.server.port + '/api'
+  // })
+
   api = axios.create({
-    baseURL: 'http://localhost:' + config.server.port + '/api'
-  })
+      baseURL: 'http://localhost:3000/api'
+    })
 
 } /*else if (rocess.env.NODE_ENV === 'production') {
   api = axios.create({
--- nuxt.config.js
+++ nuxt.config.js
@@ -80,10 +80,10 @@ export default {
     }
   },
 
-  server: {
-    port: 3000, // default: 3000
-    host: '0.0.0.0' // default: localhost
-  },
+  // server: {
+  //   port: 3000, // default: 3000
+  //   host: '0.0.0.0' // default: localhost
+  // },
 
   serverMiddleware: [
     bodyParser.json({limit: '50mb'}),
