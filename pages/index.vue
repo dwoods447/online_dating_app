e1db182ae729e258ef62fc1446491fca6daf7d59
Demaria Woods <dwoods447@gmail.com>
2020-04-28 10:57:14 -0500

    added dating tips to home page and description

--- pages/index.vue
+++ pages/index.vue
@@ -7,16 +7,26 @@
   </section>
   <hr/>
    <section class="container">
-     <h2>Dating Tips</h2>
+     
      <div class="row">
        <div class="col-lg-6">
-           <div style="max-width: 500px; max-height: 500px; margin: 0 auto;">
+           <div style="max-width: 500px; max-height: 500px;">
             <img src="~assets/img/homepage-3.jpg" alt="" style="width: 100%;">
           </div>
        </div>
        <div class="col-lg-6">
+         <h2 style="margin-left: 15px; text-align: left;">Dating Tips</h2>
           <div>
-            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+            <ul>
+              <li>First impressions are always important.</li>
+              <li>Make date plans for somewhere you'll feel comfortable.</li>
+              <li>Be confident.</li>
+              <li>Don't do all the talking.</li>
+              <li>Keep the conversation fun.</li>
+              <li>Avoid the 'ex' conversation.</li>
+              <li>Turn off your phone.</li>
+              <li>Share the bill.</li>
+            </ul>
           </div>
        </div>
      </div>
@@ -28,7 +38,7 @@
        <div class="row">
          <div class="col-lg-6">
              <div>
-              <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+              <p>Looking for that geek of your dreams that's into Fantasy, Sci-Fi, Anime, Video Games, Conventions & Cos-Play? This dating site is designed to help geeks find other geeks. Here you will find no stereotypes, no gimmicks, just pure, authentic online dating for the geek in all of us.</p>
             </div>
          </div>
           <div class="col-lg-6">
